68a09ad1262927253145a44a24b857a89c521fe6
alfredohipolito <alfredofps@gmail.com>
2024-12-16 20:06:56 -0300

    post e push pessoa

--- src/main/java/br/com/fiap/techchallenge/api/controller/PessoaController.java
+++ src/main/java/br/com/fiap/techchallenge/api/controller/PessoaController.java
@@ -1,9 +1,15 @@
 package br.com.fiap.techchallenge.api.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +33,8 @@ public class PessoaController {
      * @param size
      * @return
      */
+
+     //http://localhost:8080/pessoas?page=1&size=10
     @GetMapping
     public ResponseEntity<List<Pessoa>> findAllPessoas(
             @RequestParam("page") int page,
@@ -36,5 +44,38 @@ public class PessoaController {
         var pessoas = this.pessoasService.findAllPessoas(page, size);
         return ResponseEntity.ok(pessoas);
 
+    }
+
+    // http://localhost:8080/pessoas/1
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Pessoa>> findPessoa(
+            @PathVariable("id") Long id) {
+        var pessoas = this.pessoasService.findPessoaById(id);
+        return ResponseEntity.ok(Optional.ofNullable(pessoas));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> savePessoa(
+            @RequestBody Pessoa pessoa) {
+        this.pessoasService.savePessoa(pessoa);
+        return ResponseEntity.status(201).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updatePessoa(
+            @PathVariable("id") Long id,
+            @RequestBody Pessoa pessoa) {
+        this.pessoasService.updatePessoa(pessoa, id);
+        return ResponseEntity.noContent().build();
+
+    }
+
+        @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePessoa(
+            @PathVariable("id") Long id
+
+    ) {
+        this.pessoasService.delete(id);
+        return ResponseEntity.ok().build();
     }
 }
--- src/main/java/br/com/fiap/techchallenge/infrastructure/services/PessoasService.java
+++ src/main/java/br/com/fiap/techchallenge/infrastructure/services/PessoasService.java
@@ -3,6 +3,7 @@ package br.com.fiap.techchallenge.infrastructure.services;
 import br.com.fiap.techchallenge.domain.model.Pessoa;
 import br.com.fiap.techchallenge.domain.repository.PessoaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 import java.util.List;
 
@@ -21,4 +22,26 @@ public class PessoasService {
         return this.pessoaRepository.findAll(size, offset);
     }
 
+    public Pessoa findPessoaById(Long id) {
+        return this.pessoaRepository.findById(id).orElse(null);
+    }
+
+    public void savePessoa(Pessoa pessoa) {
+        var save = this.pessoaRepository.save(pessoa);
+        Assert.state(save == 1, "Erro ao salvar pessoa" + pessoa.getNome());
+    }
+
+    public void updatePessoa(Pessoa pessoa, Long id) {
+        var update = this.pessoaRepository.update(pessoa, id);
+        if (update == 0) {
+            throw new RuntimeException("Pessoa nao encontrada");
+        }
+    }
+
+    public void delete(Long id) {
+        var delete = this.pessoaRepository.delete(id);
+        if (delete == 0) {
+            throw new RuntimeException("Pessoa nao encontrada");
+        }
+    }
 }
