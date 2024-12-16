6af4ac56965b66b1a3fe979b0c1a1a763067bed1
alfredohipolito <alfredofps@gmail.com>
2024-12-16 18:41:05 -0300

    acesso ao sql lista pessoas ok

--- src/main/java/br/com/fiap/techchallenge/api/controller/PessoaController.java
+++ src/main/java/br/com/fiap/techchallenge/api/controller/PessoaController.java
@@ -1,13 +1,15 @@
 package br.com.fiap.techchallenge.api.controller;
 
 import java.util.List;
-import br.com.fiap.techchallenge.domain.model.Pessoa;
-import br.com.fiap.techchallenge.infrastructure.services.PessoasService;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.fiap.techchallenge.domain.model.Pessoa;
+import br.com.fiap.techchallenge.infrastructure.services.PessoasService;
 
 
 @RestController
@@ -20,10 +22,19 @@ public class PessoaController {
         this.pessoasService = pessoasService;
     }
 
+    /**
+     * @param page
+     * @param size
+     * @return
+     */
     @GetMapping
-    public ResponseEntity<List<Pessoa>> findAll(){
+    public ResponseEntity<List<Pessoa>> findAllPessoas(
+            @RequestParam("page") int page,
+            @RequestParam("size") int size
 
-        List<Pessoa> all = pessoasService.findAll();
-        return ResponseEntity.ok(all);
+    ) {
+        var pessoas = this.pessoasService.findAllPessoas(page, size);
+        return ResponseEntity.ok(pessoas);
+
     }
 }
--- src/main/java/br/com/fiap/techchallenge/infrastructure/services/PessoasService.java
+++ src/main/java/br/com/fiap/techchallenge/infrastructure/services/PessoasService.java
@@ -1,21 +1,24 @@
 package br.com.fiap.techchallenge.infrastructure.services;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import br.com.fiap.techchallenge.domain.model.Pessoa;
+import br.com.fiap.techchallenge.domain.repository.PessoaRepository;
 import org.springframework.stereotype.Service;
 
-import br.com.fiap.techchallenge.domain.model.Pessoa;
+import java.util.List;
 
 @Service
 public class PessoasService {
 
-    public List<Pessoa> findAll() {
-       Pessoa pessoa1 = new Pessoa();
-       pessoa1.setNome("Alfredo");
-       List listaPessoa = new ArrayList<>();
-       listaPessoa.add(pessoa1);
-       return listaPessoa;  
+    private final PessoaRepository pessoaRepository;
+
+    public PessoasService(PessoaRepository pessoaRepository) {
+        this.pessoaRepository = pessoaRepository;
+    }
+
+
+    public List<Pessoa> findAllPessoas(int page, int size) {
+        int offset = (page - 1) * size;
+        return this.pessoaRepository.findAll(size, offset);
     }
 
 }
