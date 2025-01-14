bcd07ad4296e6a2a2d79d06222b1ea7680a1ff33
Fabio Batista <fabiobatista.bertioga@gmail.com>
2025-01-14 17:58:34 -0300

    Adicionando interface da refatoração

--- src/main/java/br/com/fiap/techchallenge/service/DonoRestauranteServiceInterface.java
+++ src/main/java/br/com/fiap/techchallenge/service/DonoRestauranteServiceInterface.java
@@ -1,4 +1,33 @@
 package br.com.fiap.techchallenge.service;
 
+import br.com.fiap.techchallenge.model.DonoRestaurante;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public interface DonoRestauranteServiceInterface {
+
+    DonoRestaurante save(DonoRestaurante donoRestaurante);
+
+    DonoRestaurante updateDonoRestaurante(Long id, DonoRestaurante updatedDonoRestaurante);
+
+    DonoRestaurante findById(Long id);
+
+    DonoRestaurante findByNome(String nome);
+
+    DonoRestaurante findByEmail(String email);
+
+    List<DonoRestaurante> findAllByOrderByIdAsc();
+
+    List<DonoRestaurante> findAllByOrderByNomeAsc();
+
+    List<DonoRestaurante> findAllByOrderByEmailAsc();
+
+    List<DonoRestaurante> findAllByOrderByEnderecoAsc();
+
+    List<DonoRestaurante> findAllByOrderByDataUltimaAlteracaoAsc();
+
+    Long deleteDonoRestaurante(Long id);
 }
